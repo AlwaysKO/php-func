61907de33f90ad77efb131f70ed43cab09ab56e0
pj <pengjun@yihaohulia.com>
2019-09-29 14:59:21 +0800

    加上static

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@
 namespace alwaysKo\phpFunc;
 
 class Functions {
-    public function dump2($arr) {
+    public static function dump2($arr) {
         echo '<pre>';
         var_dump($arr);
         exit;
