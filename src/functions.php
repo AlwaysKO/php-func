53d97438238c5904606dced1ec6a0309f87877af
pj <pengjun@yihaohulia.com>
2019-09-29 14:28:02 +0800

    1

---
+++ src/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 2019-9-29
+ */
+
+namespace alwaysKo\phpFunc;
+
+class Functions {
+    public function dump2($arr) {
+        echo '<pre>';
+        var_dump($arr);
+        exit;
+    }
+}
